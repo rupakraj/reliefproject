654e0e2732718062e3ce14e702828fcea767e49d
anupshk <anup.shk@gmail.com>
2015-05-01 18:29:00 +0545

    area detail tabs basic view

---
+++ modules/area/views/admin/detail.php
@@ -0,0 +1,93 @@
+<aside class="right-side">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>Area detail</h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- row -->
+        <div class="row">
+        <div class="col-md-12">
+            <div class="box box-solid box-default">
+                <div class="box-body">
+                    <!-- Custm Tabs -->
+                    <div class="nav-tabs-custom">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab_1" data-toggle="tab">Tab 1</a></li>
+                            <li><a href="#tab_2" data-toggle="tab">Tab 2</a></li>
+                            <li><a href="#tab_3" data-toggle="tab">Tab 3</a></li>
+                            <li><a href="#tab_4" data-toggle="tab">Tab 4</a></li>
+                            <li><a href="#tab_5" data-toggle="tab">Tab 5</a></li>
+                            <li><a href="#tab_6" data-toggle="tab">Tab 6</a></li>
+                            <li><a href="#tab_7" data-toggle="tab">Tab 7</a></li>
+                            <li><a href="#tab_8" data-toggle="tab">Tab 8</a></li>
+                            <li><a href="#tab_9" data-toggle="tab">Tab 9</a></li>
+                            <li><a href="#tab_10" data-toggle="tab">Tab 10</a></li>
+                            <li><a href="#tab_11" data-toggle="tab">Tab 11</a></li>
+                            <li><a href="#tab_12" data-toggle="tab">Tab 12</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab_1">
+                                <b>How to use:</b>
+                                <p>Exactly like the original bootstrap tabs except you should use
+                                    the custom wrapper <code>.nav-tabs-custom</code> to achieve this style.</p>
+                                A wonderful serenity has taken possession of my entire soul,
+                                like these sweet mornings of spring which I enjoy with my whole heart.
+                                I am alone, and feel the charm of existence in this spot,
+                                which was created for the bliss of souls like mine. I am so happy,
+                                my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
+                                that I neglect my talents. I should be incapable of drawing a single stroke
+                                at the present moment; and yet I feel that I never was a greater artist than now.
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_2">
+                                The European languages are members of the same family. Their separate existence is a myth.
+                                For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
+                                in their grammar, their pronunciation and their most common words. Everyone realizes why a
+                                new common language would be desirable: one could refuse to pay expensive translators. To
+                                achieve this, it would be necessary to have uniform grammar, pronunciation and more common
+                                words. If several languages coalesce, the grammar of the resulting language is more simple
+                                and regular than that of the individual languages.
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_3">
+                                33333
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_4">
+                                44444
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_5">
+                                5555555
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_6">
+                                6666666
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_7">
+                                777777777
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_8">
+                                8888888888
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_9">
+                                99999
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_10">
+                                1010101010
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_11">
+                                1111111
+                            </div><!-- /.tab-pane -->
+                            <div class="tab-pane" id="tab_12">
+                                <?php echo $this->load->view('partial_area_req_items');?>
+                            </div><!-- /.tab-pane -->
+                        </div><!-- /.tab-content -->
+                    </div>
+
+                </div>
+            </div>
+        </div><!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+    </section><!-- /.content -->
+</aside><!-- /.right-side -->
