14980156332fdaafc4b83536710078ba7d4ade53
Rubim Shrestha <shrestharubim@gmail.com>
2015-05-03 16:29:54 +0545

    [UPDATE] Schedule Design Fixed

--- modules/schedule/views/admin/index.php
+++ modules/schedule/views/admin/index.php
@@ -88,12 +88,12 @@
     </script>
     <style>
     
-    body {
+    /*body {
     	margin: 40px 10px;
     	padding: 0;
     	font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
     	font-size: 14px;
-    }
+    }*/
     
     #calendar {
     	max-width: 900px;
